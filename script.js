b15c941f65b0398ae3f35c0c0da1edb0c0f7e730
choeqq <k.chola@op.pl>
2022-05-04 23:28:11 +0200

    both handles working

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector("click", (e) => {
+  let handle;
+  if (e.target.matches(".handle")) {
+    handle = e.target;
+  } else {
+    handle = e.target.closest(".handle");
+  }
+  if (handle !== null) {
+    onHandleClick(handle);
+  }
+});
+
+function onHandleClick(handle) {
+  const slider = handle.closest(".container").querySelector(".slider");
+  const sliderIndex = parseInt(
+    slider.style.getComputedStyle(slider).getPropertyValue("--slider-index")
+  );
+  if (handle.classList.contains("left-handle")) {
+    slider.style.setProperty("--slider-index", sliderIndex - 1);
+  }
+  if (handle.classList.contains("right-handle")) {
+    slider.style.setProperty("--slider-index", sliderIndex + 1);
+  }
+}
